0eaf968d8a976f1f03d053b7944eb8907021e4f6
azizyarzai <azizulrahman.yarzai@gmail.com>
2021-08-03 16:56:06 +0430

    class code 1 August

--- products/admin.py
+++ products/admin.py
@@ -7,6 +7,11 @@ from products.models import Product
 class ProductAdmin(admin.ModelAdmin):
     list_display = ["id", "name", "price", "code",
                     "quantity", "is_availible", "updated", "created"]
+    list_filter = ["price", "is_availible", "name"]
+    search_fields = ["price", "name", "code"]
+    list_per_page = 2
+    list_editable = ["is_availible", "code"]
+    # save_as_continue = False
 
 
 admin.site.register(Product, ProductAdmin)
--- products/models.py
+++ products/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -8,7 +10,17 @@ CATEGORIES = [
 ]
 
 
+def validate_name(value):
+    # print("Name validator")
+    # print("value", value)
+    if 'n' in value:
+        return value
+    # print("Hi")
+    raise ValidationError("name does not have @.")
+
+
 class Product(models.Model):
+    # name = models.CharField(max_length=150, validators=[validate_name])
     name = models.CharField(max_length=150)
     price = models.IntegerField()
     code = models.IntegerField(unique=True)
@@ -18,10 +30,21 @@ class Product(models.Model):
     category = models.CharField(max_length=150, choices=CATEGORIES)
     updated = models.DateTimeField(auto_now_add=True)
     created = models.DateTimeField(auto_now=True)
+    # email = models.EmailField()
+    slug = models.SlugField(null=True, blank=True)
 
     def __str__(self):
         return self.name + " - " + str(self.code)
 
+    def save(self, *args, **kwargs):
+        print("save method was called")
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
     class Meta:
         db_table = 'products'
         ordering = ["created", '-price']
+        # verbose_name = 'product1'
+        # verbose_name_plural = "product2"
+        # unique_together = ["name" ,"price"]
