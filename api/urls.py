52682c55ac55300b9b27276dac7f3b4ac5ba3687
azizyarzai <azizulrahman.yarzai@gmail.com>
2021-09-25 17:50:01 +0430

    serilizers

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
-from api.views import FirstApiView
+from api.views import FirstApiView, testView
 app_name = "api"
 
 # domain.com/products
 urlpatterns = [
     path("welcome/", FirstApiView.as_view()),
+    path("test/", testView),
 
 ]
--- api/views.py
+++ api/views.py
@@ -1,9 +1,37 @@
+from rest_framework import serializers
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import JsonResponse
+from api.serializers import FirstSerializer
+from rest_framework import status
+from rest_framework.decorators import api_view
+
+
+@api_view
+def testView(request):
+    return Response({"message": "Test completed."})
 
 
 class FirstApiView(APIView):
 
+    serializer_class = FirstSerializer
+
     def get(self, request, formate=None):
         return Response({"message": "Welcome to Django REST Framework"})
+
+    def post(self, request, formate=None):
+        print(type(request.data))
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            return Response({"message": f"Welcome {serializer.validated_data.get('name')}"})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, formate=None):
+        return Response({"message": "PUT request received."})
+
+    def patch(self, request, formate=None):
+        return Response({"message": "PATCH request received."})
+
+    def delete(self, request, formate=None):
+        return Response({"message": "DELETE request received."})
